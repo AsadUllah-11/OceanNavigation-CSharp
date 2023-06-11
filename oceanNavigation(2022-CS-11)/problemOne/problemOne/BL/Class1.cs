c19684544134c33bbe30cf90873879f97eff11a9
AsadUllah-11 <120157110+AsadUllah-11@users.noreply.github.com>
2023-06-11 20:17:13 +0500

    Add files via upload

---
+++ oceanNavigation(2022-CS-11)/problemOne/problemOne/BL/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemOne.BL
+{
+    class ship
+    {
+        public ship()
+        {
+
+        }
+
+        public ship(string shipNumber, string shipLocation)
+        {
+            this.shipNumber = shipNumber;
+            this.shipLocation = shipLocation;
+        }
+
+        public void printPosition(string latitudeValue, string longitudeValue)
+        {
+            Console.WriteLine("Ship is {0} and {1}", latitudeValue,longitudeValue);
+        }
+            
+        public void printSerialNumber()
+        {
+            Console.WriteLine("Ship's serial number is " + shipNumber);
+        }
+        public string shipNumber;
+        public string shipLocation;
+
+    }
+    class LatitudeAngle
+    {
+        public LatitudeAngle()
+        {
+
+        }
+        public LatitudeAngle(int degrees, float minutes, char direction)
+        {
+            this.degrees = degrees;
+            this.minutes = minutes;
+            this.directiion = direction;
+        }
+        public string changeLatitudeAngleValue()
+        {
+            string angle = degrees + "\u00b0" + minutes + "' " + directiion;           
+            return angle;
+        }
+        public int degrees;
+        public float minutes;
+        public char directiion;        
+    }
+    class LongitudeAngle
+    {
+        public LongitudeAngle()
+        {
+
+        }
+        public LongitudeAngle(int degrees, float minutes, char direction)
+        {
+            this.degrees = degrees;
+            this.minutes = minutes;
+            this.directiion = direction;
+        }
+        public string changeLongitudeAngleValue()
+        {
+            string angle = degrees + "\u00b0" + minutes + "' " + directiion;
+            return angle;
+        }
+        public int degrees;
+        public float minutes;
+        public char directiion;
+    }
+}
---
+++ oceanNavigation(2022-CS-11)/problemOne/problemOne/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using problemOne.BL;
+
+namespace problemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ship> shipsData = new List<ship>();
+            char option = menu();
+            do
+            {
+                if (option == 'a')
+                {
+                    // Taking Input for Add A New Ship
+                    ship shipsInfo = new ship();
+                    Console.Write("Enter Ship Number: ");
+                    shipsInfo.shipNumber = Console.ReadLine();
+
+
+                    Console.WriteLine("Enter Ship Latitude: ");
+                    int latitudeDegree;
+                    float latitudeMinute;
+                    char latitudeDirection;
+
+                    Console.Write("Enter Latitudes Degree: ");
+                    latitudeDegree = int.Parse(Console.ReadLine());
+                    Console.Write("Enter Latitudes Minute: ");
+                    latitudeMinute = float.Parse(Console.ReadLine());
+                    Console.Write("Enter Latitudes Direction: ");
+                    latitudeDirection = char.Parse(Console.ReadLine());
+                    LatitudeAngle checkLatitude = new LatitudeAngle(latitudeDegree, latitudeMinute, latitudeDirection);
+                    string latitudeValue = checkLatitude.changeLatitudeAngleValue();
+
+                    Console.WriteLine("Enter Ship Longitude: ");
+                    int longitudeDegree;
+                    float longitudeMinute;
+                    char longitudeDirection;
+
+                    Console.Write("Enter Logitudes Degree: ");
+                    longitudeDegree = int.Parse(Console.ReadLine());
+                    Console.Write("Enter Longitudes Minute: ");
+                    longitudeMinute = float.Parse(Console.ReadLine());
+                    Console.Write("Enter Longitudes Direction: ");
+                    longitudeDirection = char.Parse(Console.ReadLine());
+
+                    LongitudeAngle checkLongitude = new LongitudeAngle(longitudeDegree, longitudeMinute, longitudeDirection);
+                    string longitudeValue = checkLongitude.changeLongitudeAngleValue();
+
+                    string finalDirection = latitudeValue + " " + longitudeValue;
+                    shipsInfo.shipLocation = finalDirection;
+                    shipsData.Add(shipsInfo);
+                    Console.Clear();
+                    option = menu();
+                }
+                else if (option == 'b')
+                {
+                    string shipNum;
+                    Console.Write("Enter Ship Serial Number to find its position: ");
+                    shipNum = Console.ReadLine();
+                    int idx = 0;
+                    for (int x = 0; x<shipsData.Count; x++)
+                    {
+                        if (shipNum == shipsData[x].shipNumber )
+                        {
+                            idx = x;
+                            break;
+                        }
+                    }
+                    Console.Write("Ship is at " + shipsData[idx].shipLocation);
+                }
+            }
+            while (option == 'e');
+
+            Console.ReadLine();
+        }
+        static char menu()
+        {
+            char choice;
+            Console.WriteLine("a. Add Ship");
+            Console.WriteLine("b. View Ship Position");
+            Console.WriteLine("c. View Ship Serial Number");
+            Console.WriteLine("d. Change Ship Position");
+            Console.WriteLine("e. Exit");
+            Console.Write("Enter Your Choice...");
+            choice = char.Parse(Console.ReadLine());
+            return choice;
+        }
+    }
+}
